155fe2a3220786cfbe13eb60dfd3aac6386e4092
Michael Chung <mikechung89@gmail.com>
2022-02-28 16:39:39 -0500

    commit tree e530ac3c6dc68cc63e064a21071b4c288a68f4bd

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Your code here
+const dodger = document.getElementById(`dodger`)
+document.addEventListener(`keydown`, function (event) {
+    if (event.key === `ArrowLeft`) {
+        moveDodgerLeft()
+    }
+    if (event.key ===`ArrowRight`) {
+        moveDodgerRight()
+    }
+})
+
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace(`px`, ``);
+    const left = parseInt(leftNumbers, 10);
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`
+    }
+}
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace(`px`, ``);
+    const right = parseInt(leftNumbers, 10);
+    if (right < 360) {
+        dodger.style.left = `${right + 1}px`
+    }
+}
